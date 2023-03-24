9e840937d1b9ffa6adaaf57c5e81d1b891750644
Kevin <kevin.paez1010@gmail.com>
2023-03-23 20:02:48 -0600

    consult api favorites cats and replace grid for flex

--- 4-favorites/main.js
+++ 4-favorites/main.js
@@ -1,5 +1,5 @@
 import { API_KEY } from '../config.js';
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1';
 // const img = document.querySelector('img');
 // const btn = document.getElementById('btn-change');
 
@@ -10,12 +10,12 @@ const inputNumber = document.getElementById('input-number');
 const imgContainer = document.querySelector('.container__img');
 const imgFragment = document.createDocumentFragment();
 
-getCatAwait();
-
 // ASYNC
 async function getCatAwait(limit = 1) {
   try {
-    const res = await fetch(`${API_URL}?limit=${limit}&api_key=${API_KEY}`);
+    const res = await fetch(
+      `${API_URL}/images/search?limit=${limit}&api_key=${API_KEY}`
+    );
     const data = await res.json();
 
     data.forEach((el) => {
@@ -23,10 +23,14 @@ async function getCatAwait(limit = 1) {
       const imgElement = document.createElement('img');
       const btn = document.createElement('button');
 
+      // Image Properties
       imgElement.setAttribute('src', `${el.url}`);
       imgElement.setAttribute('alt', 'Cats');
       imgElement.setAttribute('id', `${el.id}`);
+
+      // Button Properties
       btn.innerText = 'Add to Favorites';
+      btn.classList.add('btn-favorites');
 
       divImgContainer.appendChild(imgElement);
       divImgContainer.appendChild(btn);
@@ -36,7 +40,6 @@ async function getCatAwait(limit = 1) {
     });
     imgContainer.appendChild(imgFragment);
     // document.body.appendChild(imgContainer);
-    // console.log(data);
   } catch (error) {
     console.log(error);
   }
@@ -56,3 +59,16 @@ btnSend.addEventListener('click', () => {
     console.log('this is not a number');
   }
 });
+
+async function loadFavoritesCats() {
+  try {
+    const res = await fetch(`${API_URL}/favourites?limit=2&api_key=${API_KEY}`);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getCatAwait();
+loadFavoritesCats();
